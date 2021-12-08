f11474b4a46546caa8aae43097b898c54a04ad4a
Yoyo Peng <yoyopyeah@gmail.com>
2021-12-07 21:56:21 -0500

    Update unittest

---
+++ test/test_files_exist.py
@@ -0,0 +1,43 @@
+import unittest
+from pathlib import Path
+import os, sys
+parentdir = Path(__file__).parents[1]
+sys.path.append(parentdir)
+
+
+class FilesExistTest(unittest.TestCase):
+    def setUp(self):
+        self.collect_file_path = os.path.join(parentdir, 'src', 'compile_word_counts.py')
+        self.compute_title_lengths_file_path = os.path.join(parentdir, 'src', 'compute_pony_lang.py')
+        self.collect_relationships_file_path = os.path.join(parentdir, 'test', 'test_tasks.py')
+        self.sample1_file_path = os.path.join(parentdir, 'data', 'word_counts.json')
+        
+        # print("\nRUNNING TESTS FOR HW8")
+
+    def test_word_count(self):
+        print("\nEnsure compile_word_counts.py file exists")
+        self.assertEqual(os.path.isfile(self.collect_file_path), True)
+        print("OK")
+
+        
+    def test_compute_lang(self):
+        print("\nEnsure compute_pony_lang.py file exists")
+        self.assertEqual(os.path.isfile(self.compute_title_lengths_file_path), True)
+        print("OK")
+
+    def test_file_test(self):
+        print("\nEnsure test_tasks.py file exists")
+        self.assertEqual(os.path.isfile(self.collect_relationships_file_path), True)
+        print("OK")
+    
+    def test_data_files_exist(self):    
+        print("\nEnsure all the required data files are present files exist")
+        self.assertEqual(os.path.isfile(self.sample1_file_path), True)
+        print("OK")
+
+    def tearDownClass():
+        pass
+        # print("\n\nYou are all set! <3")
+    
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_tasks.py
@@ -0,0 +1,41 @@
+import unittest
+from pathlib import Path
+import os, sys
+parentdir = Path(__file__).parents[1]
+sys.path.append(parentdir)
+from src.compile_word_counts import *
+from src.compute_pony_lang import *
+
+
+class TasksTest(unittest.TestCase):
+    def setUp(self):
+        dir = os.path.dirname(__file__)
+        self.mock_dialog = os.path.join(dir, 'fixtures', 'mock_dialog.csv')
+        self.true_word_counts = os.path.join(dir, 'fixtures', 'word_counts.true.json')
+        self.true_tf_idfs = os.path.join(dir, 'fixtures', 'tf_idfs.true.json')
+        
+        
+    def test_task1(self):
+        # use  self.mock_dialog and self.true_word_counts; REMOVE self.assertTrue(True) and write your own assertion, i.e. self.assertEquals(...)
+        print(f"\nTesting functionality of compile_word_counts.py")
+        wcounts = count_word(self.mock_dialog)
+        with open(self.true_word_counts) as f:
+            wc_true = json.load(f)
+        self.assertEqual(wcounts, wc_true)
+        print("OK")
+
+
+    def test_task2(self):
+        # use self.true_word_counts self.true_tf_idfs; REMOVE self.assertTrue(True) and write your own assertion, i.e. self.assertEquals(...)
+        print(f"\nTesting functionality of compute_pony_lang.py")
+        with open(self.true_word_counts) as f:
+            wcounts = json.load(f)
+        tfidf = analyze_lang(wcounts, 5)
+        with open(self.true_tf_idfs) as f:
+            tfidf_true = json.load(f)
+        self.assertEqual(tfidf, tfidf_true)
+        print("OK")
+        
+    
+if __name__ == '__main__':
+    unittest.main()
